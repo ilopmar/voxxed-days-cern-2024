a412f9f723c1952cbda84d48df961884daf08e36
Iván López <lopez.ivan@gmail.com>
2024-01-22 23:23:33 +0100

    Add live-coding code during talk

--- src/test/java/com/voxxeddays/cern/AbstractContainerTest.java
+++ src/test/java/com/voxxeddays/cern/AbstractContainerTest.java
@@ -6,13 +6,46 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(
     webEnvironment = WebEnvironment.RANDOM_PORT
+//    properties = "spring.datasource.url=jdbc:tc:postgresql:16.1:///"
 )
+//@Testcontainers
 public abstract class AbstractContainerTest {
+
+//  @Container
+//  @ServiceConnection
+  static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(
+      DockerImageName.parse("postgres:16.1")
+  );
+
+//  @Container
+//  @ServiceConnection
+  static KafkaContainer kafkaContainer = new KafkaContainer(
+      DockerImageName.parse("confluentinc/cp-kafka:7.5.3")
+  );
+
+  @DynamicPropertySource
+  static void setupContainers(DynamicPropertyRegistry registry){
+    Startables.deepStart(postgreSQLContainer, kafkaContainer).join();
+
+    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+  }
 
   protected RequestSpecification requestSpecification;
 
